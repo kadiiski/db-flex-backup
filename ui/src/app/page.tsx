171bb4fdca056e41f59e637bd031ca3641f6df35
Pavel Tsandev <pavel.tsandev@pfizer.com>
2025-07-10 17:07:44 +0300

    Remove fallback values for retention and cron.

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -5,8 +5,8 @@ import HomePageClient from "@/app/HomePageClient";
 
 export default function HomePage() {
   const pageTitle = process.env.BACKUPS_UI_TITLE || "Database Backups";
-  const retention = process.env.RETENTION_COUNT || "7";
-  const cronSchedule = process.env.CRON_SCHEDULE || "0 0 * * *";
+  const retention = process.env.RETENTION_COUNT!;
+  const cronSchedule = process.env.CRON_SCHEDULE!;
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <HomePageClient pageTitle={pageTitle} retention={retention} cronSchedule={cronSchedule} />
