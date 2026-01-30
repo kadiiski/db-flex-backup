23227b673a6200096a8ea6cadef930704e5fa553
Miroslav Banov <miroslav.banov@pfizer.com>
2026-01-30 13:17:35 +0200

    Increase max upload size 10mb -> 100mb

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // Since the latest update to nextjs of this project, I cannot upload more than 10mb database.
+    middlewareClientMaxBodySize: '100mb',
+  },
 };
 
 export default nextConfig;
